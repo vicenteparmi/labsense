5496c7fb2fbc9c18ce87c94fe94af95ab730005f
Vicente Parmigiani <vicenteparmigiani@gmail.com>
2024-05-02 15:48:43 -0300

    Experiment view redesign

--- lib/pages/experiments/experiment_view.dart
+++ lib/pages/experiments/experiment_view.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 import 'package:labsense/pages/main_pages/home.dart';
 import 'package:labsense/scripts/database.dart';
@@ -88,224 +90,201 @@ class _ExperimentViewContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GestureDetector(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title:
-                          Text(AppLocalizations.of(context)!.experimentTitle),
-                      content: Text(title),
-                      actions: <Widget>[
-                        TextButton(
-                          child: Text(AppLocalizations.of(context)!.close),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-              child: Text(
-                title,
-                style: Theme.of(context).textTheme.titleLarge,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            Text(
-              AppLocalizations.of(context)!.lastUpdated(
-                  DateFormat('dd/MM/yyyy').format(DateTime.parse(createdTime))),
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-          ],
-        ),
-        actions: [
-          PopupMenuButton<int>(
-            itemBuilder: (context) => [
-              PopupMenuItem(
-                value: 1,
-                child: Text(AppLocalizations.of(context)!.edit),
-              ),
-              PopupMenuItem(
-                value: 2,
-                child: Text(AppLocalizations.of(context)!.delete),
-                onTap: () {
-                  // Delete this record from the database
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title:
-                            Text(AppLocalizations.of(context)!.confirmDelete),
-                        content: Text(AppLocalizations.of(context)!
-                            .confirmExperimentDelete),
-                        actions: <Widget>[
-                          TextButton(
-                            child: Text(AppLocalizations.of(context)!.cancel),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          TextButton(
-                            child: Text(AppLocalizations.of(context)!.delete),
-                            onPressed: () {
-                              openMyDatabase().then((value) => value.delete(
-                                    'experiments',
-                                    where: 'id = ?',
-                                    whereArgs: [experimentId.toString()],
-                                  ));
-                              Navigator.of(context).pop();
-                              // Push and remove all previous routes
-                              Navigator.of(context).pushAndRemoveUntil(
-                                MaterialPageRoute(
-                                  builder: (context) => const Home(),
-                                ),
-                                (route) => false,
-                              );
-                            },
-                          ),
-                        ],
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: 225.0,
+            actions: [
+              PopupMenuButton<int>(
+                itemBuilder: (context) => [
+                  PopupMenuItem(
+                    value: 1,
+                    child: Text(AppLocalizations.of(context)!.edit),
+                  ),
+                  PopupMenuItem(
+                    value: 2,
+                    child: Text(AppLocalizations.of(context)!.delete),
+                    onTap: () {
+                      // Delete this record from the database
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text(
+                                AppLocalizations.of(context)!.confirmDelete),
+                            content: Text(AppLocalizations.of(context)!
+                                .confirmExperimentDelete),
+                            actions: <Widget>[
+                              TextButton(
+                                child:
+                                    Text(AppLocalizations.of(context)!.cancel),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                              TextButton(
+                                child:
+                                    Text(AppLocalizations.of(context)!.delete),
+                                onPressed: () {
+                                  openMyDatabase().then((value) => value.delete(
+                                        'experiments',
+                                        where: 'id = ?',
+                                        whereArgs: [experimentId.toString()],
+                                      ));
+                                  Navigator.of(context).pop();
+                                  // Push and remove all previous routes
+                                  Navigator.of(context).pushAndRemoveUntil(
+                                    MaterialPageRoute(
+                                      builder: (context) => const Home(),
+                                    ),
+                                    (route) => false,
+                                  );
+                                },
+                              ),
+                            ],
+                          );
+                        },
                       );
                     },
-                  );
+                  ),
+                ],
+                onSelected: (value) {
+                  if (value == 1) {
+                    // Navigate to the edit page
+                  } else if (value == 2) {
+                    // Delete the experiment
+                  }
                 },
               ),
             ],
-            onSelected: (value) {
-              if (value == 1) {
-                // Navigate to the edit page
-              } else if (value == 2) {
-                // Delete the experiment
-              }
-            },
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        child: Column(
-          children: [
-            // Experiment description
-            Card.outlined(
-              margin: const EdgeInsets.only(bottom: 12.0),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+            flexibleSpace: FlexibleSpaceBar(
+              title: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
                     children: [
-                      Text(
-                        AppLocalizations.of(context)!.experimentBriefDesc,
-                        style: Theme.of(context).textTheme.titleMedium,
+                      TextSpan(
+                        text: title,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall!
+                            .copyWith(fontSize: 16.0),
                       ),
-                      const SizedBox(height: 8.0),
-                      Text(
-                        description,
-                        style: Theme.of(context).textTheme.bodyMedium,
+                      TextSpan(
+                        text:
+                            '\n${AppLocalizations.of(context)!.lastUpdated(DateFormat('dd/MM/yyyy').format(DateTime.parse(createdTime)))}',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium!
+                            .copyWith(fontSize: 10.0),
                       ),
                     ],
+                  )),
+              background: Padding(
+                padding: const EdgeInsets.all(80.0),
+                child: CircleAvatar(
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  radius: 48.0,
+                  child: Icon(
+                    Icons.science_outlined,
+                    size: 48.0, // Reduced size
+                    color: Theme.of(context).colorScheme.onPrimary,
                   ),
                 ),
               ),
+              collapseMode: CollapseMode.pin,
+              centerTitle: true,
+              titlePadding: const EdgeInsetsDirectional.only(bottom: 16.0),
             ),
-            // Procedures
-            Card.outlined(
-              margin: const EdgeInsets.only(bottom: 12.0),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          AppLocalizations.of(context)!.procedures,
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.add_box_outlined)),
-                      ],
-                    ),
-                    const SizedBox(height: 8.0),
-                    StepTracker(
-                      unSelectedColor: Theme.of(context).colorScheme.primary,
-                      dotSize: 12.0,
-                      stepTrackerType: StepTrackerType.indexedVertical,
-                      steps: steps
-                          .map(
-                            (step) => Steps(
-                              title: step['title'],
-                              description: step['brief_description'],
-                            ),
-                          )
-                          .toList(),
-                    ),
-                    if (steps.isEmpty)
-                      Text(
-                        AppLocalizations.of(context)!.noProcedures,
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      ),
-                  ],
-                ),
-              ),
-            ),
-            // Results
-            Card.outlined(
-              margin: const EdgeInsets.only(bottom: 12.0),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                ExpansionTile(
+                  title: Text(
+                    AppLocalizations.of(context)!.experimentBriefDesc,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  subtitle: Text(
+                    AppLocalizations.of(context)!.clickToAlternate,
+                  ),
+                  leading: const Icon(Icons.description_outlined),
+                  collapsedShape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  childrenPadding: const EdgeInsets.only(
+                      left: 24.0, right: 24.0, bottom: 24.0),
+                  children: <Widget>[
                     Text(
-                      AppLocalizations.of(context)!.results,
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    const SizedBox(height: 8.0),
-                    // List of results
-                    ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: 3,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          margin: const EdgeInsets.only(bottom: 8.0),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Result $index',
-                                  style: Theme.of(context).textTheme.titleSmall,
-                                ),
-                                const SizedBox(height: 8.0),
-                                Text(
-                                  'Result description',
-                                  style: Theme.of(context).textTheme.bodyMedium,
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
+                      description,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                      textAlign: TextAlign.justify,
                     ),
                   ],
                 ),
+                // Creation date
+                ListTile(
+                  title: Text(AppLocalizations.of(context)!.creationDate),
+                  subtitle: Text(
+                    DateFormat('dd/MM/yyyy')
+                        .format(DateTime.parse(createdTime)),
+                  ),
+                  leading: const Icon(Icons.access_time_rounded),
+                  titleTextStyle: Theme.of(context).textTheme.titleMedium,
+                ),
+              ],
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                AppLocalizations.of(context)!.procedures,
+                style: Theme.of(context).textTheme.titleLarge,
               ),
             ),
-          ],
-        ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount:
+                    (MediaQuery.of(context).orientation == Orientation.portrait)
+                        ? 2
+                        : 4,
+                crossAxisSpacing: 0,
+                mainAxisSpacing: 0,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Result $index',
+                            style: Theme.of(context).textTheme.titleSmall,
+                          ),
+                          const SizedBox(height: 8.0),
+                          Text(
+                            'Result description',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                childCount: 3,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
